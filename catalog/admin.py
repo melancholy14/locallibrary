f19b2d6bde62c65b4e7b43925f1897eb504b3f72
Misol Goh <melancholy8914@gmail.com>
2020-11-14 00:12:58 +0000

    update: update BookInstance list view

--- catalog/admin.py
+++ catalog/admin.py
@@ -8,30 +8,37 @@ from .models import Author, Book, BookInstance, Genre
 admin.site.register(Genre)
 # admin.site.register(BookInstance)
 
+class BooksInline(admin.StackedInline):
+  """Declare inlines, of type TabularInline (horizontal layout) or StackedInline (vertical layout)"""
+  model = Book
+
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
   list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
   fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+  inlines = [BooksInline]
+  
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
 
 
-# Register the Admin classes for Book using the decorator
 class BooksInstanceInline(admin.TabularInline):
   model = BookInstance
 
 
-# Register the Admin classes for BookInstance using the decorator
+# Register the Admin classes for Book using the decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
   list_display = ('title', 'author', 'display_genre')
   inlines = [BooksInstanceInline]
 
 
+# Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
   list_filter = ('status', 'due_back')
+  list_display = ('book', 'status', 'due_back', 'display_id')
 
   fieldsets = (
     (None, {
