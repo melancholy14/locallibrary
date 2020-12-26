3d8e3c6f80608f795723fd96f22e18a466162cc9
Misol Goh <melancholy8914@gmail.com>
2020-12-26 19:36:01 +0000

    update: use generic views for create/edit/delete func

--- catalog/urls.py
+++ catalog/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
   path('author/<int:pk>', views.author_detail_view, name='author-detail'),
   path('mybooks/', views.LoanedBooksbyUserListView.as_view(), name='my-borrowed'),
   path('borrowed/', views.LoanedBooksByAllUserListView.as_view(), name='all-borrowed'),
+  path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+  path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+  path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -2,11 +2,13 @@ import datetime
 
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views import generic
 
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.auth.decorators import login_required, permission_required
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Create your views here.
 from catalog.models import Book, Author, BookInstance, Genre
@@ -140,4 +142,20 @@ def renew_book_librarian(request, pk):
   }
 
   return render(request, 'catalog/book_renew_librarian.html', context)
+
+
+class AuthorCreate(CreateView):
+  model = Author
+  fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+  initial = { 'date_of_death': '11/06/2020' }
+
+
+class AuthorUpdate(UpdateView):
+  model = Author
+  fields = '__all__' # Not recommended (potential security issue if more fields added)
+
+
+class AuthorDelete(DeleteView):
+  model = Author
+  success_url = reverse_lazy('authors')
 
